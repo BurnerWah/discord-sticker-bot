ca82cf84ff7e1c12e9cc6f17655df8b2d9462ba5
Burner <burner@burner.gay>
2022-07-03 23:02:10 -0500

    Added check to verify that sticker exists

--- src/stickerCommand.ts
+++ src/stickerCommand.ts
@@ -9,10 +9,10 @@ function normalize(str: string): string {
   return str.toLowerCase().trim().replaceAll(/\s+/g, '-')
 }
 
-export function onStickerCommand(
+export async function onStickerCommand(
   ctx: Context<string, Env>,
   interaction: Interaction,
-): Response {
+): Promise<Response> {
   const options = interaction.data?.options
   console.log(`Options: ${JSON.stringify(options)}`)
   if (options) {
@@ -21,12 +21,26 @@ export function onStickerCommand(
     if (raw_character && raw_sticker) {
       const character = normalize(raw_character)
       const sticker = normalize(raw_sticker)
-      return ctx.json({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          content: `https://wah.rest/sticker/${character}/${sticker}`,
-        },
-      })
+
+      const stickers = await fetch(`https://wah.rest/sticker/${character}/list`)
+      const stickers_list: string[] = await stickers.json()
+
+      if (stickers_list.includes(sticker)) {
+        return ctx.json({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
+            content: `https://wah.rest/sticker/${character}/${sticker}`,
+          },
+        })
+      } else {
+        return ctx.json({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
+            content: `Sticker not found`,
+            flags: InteractionResponseFlags.EPHEMERAL,
+          },
+        })
+      }
     }
   }
   return ctx.json({
